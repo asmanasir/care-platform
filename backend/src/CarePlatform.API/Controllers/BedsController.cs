8726aef9472b171d077019d974f7637ef8da5960
asma hafeez <33054727+asmanasir@users.noreply.github.com>
2026-01-15 15:30:19 +0100

    Wire Capacity module into API with Swagger and persistence

---
+++ backend/src/CarePlatform.API/Controllers/BedsController.cs
@@ -0,0 +1,23 @@
+﻿using Capacity.Application.Commands.OccupyBed;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarePlatform.API.Controllers;
+
+[ApiController]
+[Route("api/beds")]
+public class BedsController : ControllerBase
+{
+    private readonly OccupyBedCommandHandler _handler;
+
+    public BedsController(OccupyBedCommandHandler handler)
+    {
+        _handler = handler;
+    }
+
+    [HttpPost("{id}/occupy")]
+    public async Task<IActionResult> Occupy(Guid id)
+    {
+        await _handler.Handle(new OccupyBedCommand(id));
+        return Ok();
+    }
+}
